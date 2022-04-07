d23838acb2e6f5ff794a84108cc2daeda1696d5c
Isaque Lemos <88510197+IsaqueLemos@users.noreply.github.com>
2022-04-07 20:32:25 -0300

    Atualização do Trabalho

--- V1.py
+++ V1.py
@@ -1,35 +1,101 @@
 from tkinter import *
+from tkinter import font
+from tkinter.ttk import Combobox
+
+arquivo = open('dados.txt', 'a')
+arquivo.close()
+
+def verificar():
+  dados_cadastrados = cadastrados[tb.get()]
+  lb_anolancamento.configure(text=f'Ano de Lançamento: {dados_cadastrados[0]}')
+  lb_nomedabanda.configure(text=f'Nome da banda: {dados_cadastrados[1]}')
+  lb_lancamento.configure(text=f'Álbum Lançamento: {dados_cadastrados[2]}')
+
+def cadastrar_album():
+  nome_album = txtalb.get()
+  ano_album = txtano.get()
+  nome_banda = txtband.get()
+  lancamento = tb_tela1.get()
+
+  arquivo = open('dados.txt', 'a')
+  arquivo.write(f'{nome_album}%{ano_album}%{nome_banda}%{lancamento}\n')
+  arquivo.close()
+
+  window.destroy()
+  tela_dados_salvos()
+
+def tela_dados_salvos():
+  global lb_lancamento, lb_nomedabanda, lb_anolancamento, cadastrados, tb
+
+  ### Configuraçao da janela
+  janela2 = Tk()
+  janela2.title('Dados Salvos')
+  janela2.geometry('500x500')
+
+  ### Lendo e agrupando dados 
+  arquivo = open('dados.txt', 'r')
+  dados = arquivo.readlines()
+  nomes = []
+  cadastrados = {}
+  for album in dados:
+      album = album.split('%')
+      nomes.append(album[0])
+      cadastrados.setdefault(album[0], album[1:])
+  arquivo.close()
+
+  ### Lista de Dados na Interface
+  Label(janela2, text='Álbuns Cadastrados', font='arial 14').place(x=180, y=50)
+  tb = Combobox(janela2, value=nomes, font='arial 14')
+  tb.place(x=25, y=125)
+
+  ### butao
+  Button(janela2, text='Verificar', font='arial 14', command=verificar).place(x=300, y=120)
+
+  ### Ano de lancamento
+  lb_anolancamento = Label(janela2, text='Ano de Lançamento: ', font='arial 14')
+  lb_anolancamento.place(x=25, y=200)
+
+  ### Nome da Banda 
+  lb_nomedabanda = Label(janela2, text='Nome da Banda: ', font='arial 14')
+  lb_nomedabanda.place(x=25, y=250)
+
+  ## Lançamento
+  lb_lancamento = Label(janela2, text='Álbum Lançamento: ', font='arial 14')
+  lb_lancamento.place(x=25, y=300)
+
+  janela2.mainloop()
 
 window = Tk()
 window.title('Albuns')
-window.geometry("500x500")
+window.geometry('500x500')
 
-lblalb = Label(window, text= 'Informações do Album')
+### tela cadastro
+lblalb = Label(window, text= 'Informações do Album', font='arial 14')
 lblalb.place(x = 200, y = 50)
 
-lblalb = Label(window, text= 'Nome do Album')
+lblalb = Label(window, text= 'Nome do Album:', font='arial 14')
 lblalb.place(x = 20, y = 175)
-txtalb = Entry(window, text = 'Nome do Album', bd = 5)
+txtalb = Entry(window,  bd = 5, width=40)
 txtalb.place(x = 20, y = 200)
 
-lblano = Label(window, text= 'Ano do Album')
+lblano = Label(window, text= 'Ano do Album:', font='arial 14')
 lblano.place(x = 20, y = 225)
-txtano = Entry(window, text = 'Ano do Album', bd = 5)
+txtano = Entry(window, bd = 5, width=40)
 txtano.place(x = 20, y = 250)
 
-lblband = Label(window, text= 'Nome da Banda/Artista')
+lblband = Label(window, text= 'Nome da Banda/Artista:', font='arial 14')
 lblband.place(x = 20, y = 275)
-txtband = Entry(window, text = 'Nome da Banda', bd = 5)
+txtband = Entry(window, bd = 5, width=40)
 txtband.place(x = 20, y = 300)
 
-lblband = Label(window, text= 'Album lançamento?')
+lblband = Label(window, text= 'Album lançamento?', font='arial 14')
 lblband.place(x = 20, y = 350)
 
-v0=IntVar()
-v0.set(1)
-r1=Radiobutton(window, text="Sim", variable=v0, value=1)
-r2=Radiobutton(window, text="Não", variable=v0, value=2)
-r1.place(x=20,y=380)
-r2.place(x=80, y=380)
+tb_tela1 = Combobox(window, values=['Sim', 'Nao'], font='arial 14')
+tb_tela1.place(x=20, y=380)
+
+botao_cadastro = Button(text="Cadatrar Álbum", font='arial 14', command=cadastrar_album)
+botao_cadastro.place(x = 20, y = 420)
+
 
 window.mainloop()
---
+++ v2_emDesenvolvimento.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from tkinter.ttk import Combobox
+
+def solicitar():
+    fatorDeBusca = busca.get()
+
+    arquivo = open('dados.txt', 'r')
+    dados = arquivo.readlines()
+    nome_do_artista = []
+    cadastrados = {}
+    for album in dados:
+        album = album.split('%')
+        nome_do_artista.append(album[2])
+        cadastrados.setdefault(album[2], [album[0], album[1], album[3]])
+    arquivo.close()
+
+    print(cadastrados)
+
+    albuns_achados = []
+    for nome in nome_do_artista:
+        print(fatorDeBusca in nome)
+        print(nome)
+        if fatorDeBusca in nome:
+            albuns_achados.append(cadastrados[nome][0])
+
+    print(albuns_achados)
+
+    tb_dados.configure(values=albuns_achados)
+
+
+
+janela = Tk()
+janela.geometry('500x500')
+
+Label(janela, text='Buscar por banda/artista').pack()
+busca = Entry(janela, bd=5)
+busca.pack()
+
+
+tb_dados = Combobox(janela)
+tb_dados.pack()
+
+Button(janela, text='Buscar', command=solicitar).pack()
+
+
+janela.mainloop()

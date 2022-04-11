2bbd046ca1eb13f778e7a0ec9252cec3bd552298
Isaque Lemos <88510197+IsaqueLemos@users.noreply.github.com>
2022-04-11 20:47:12 -0300

    Finalizando v2.py

--- V2.py
+++ V2.py
@@ -4,70 +4,88 @@ from tkinter.ttk import Combobox
 arquivo = open('dados.txt', 'a')
 arquivo.close()
 
-def verificar():
+### Banco de Dados
+def buscar_dados():
+  arquivo = open('dados.txt', 'r')
+  dados = arquivo.readlines()
+  albuns = []
+  arquivo.close()
+  for album in dados:
+    album = album.split('%')
+    albuns.append(album)
+  return albuns
+
+### Banco de Dados
+def gravar_dados(texto):
+  arquivo = open('dados.txt', 'a')
+  arquivo.write(texto)
+  arquivo.close()
+
+### Regra de Negócio
+def verificar_album():
   dados_cadastrados = cadastrados[tb.get()]
   lb_anolancamento.configure(text=f'Ano de Lançamento: {dados_cadastrados[0]}')
   lb_nomedabanda.configure(text=f'Nome da Banda: {dados_cadastrados[1]}')
   lb_lancamento.configure(text=f'Álbum Lançamento: {dados_cadastrados[2]}')
 
+### 
+def coletar_albuns_cadastrados():
+  data = buscar_dados()
+  nomes = []
+  cadastrados = {}
+  for album in data:
+    nomes.append(album[0])
+    cadastrados.setdefault(album[0], album[1:])
+  return [nomes, cadastrados]
+
+### Regra de Negócio
 def cadastrar_album():
   nome_album = txtalb.get()
   ano_album = txtano.get()
   nome_banda = txtband.get()
   lancamento = opcaoLancamento.get()
 
-  arquivo = open('dados.txt', 'a')
   if lancamento == 1:
-    arquivo.write(f'{nome_album}%{ano_album}%{nome_banda}%Sim\n')
+    gravar_dados(f'{nome_album}%{ano_album}%{nome_banda}%Sim\n')
   else:
-    arquivo.write(f'{nome_album}%{ano_album}%{nome_banda}%Nao\n')
-  arquivo.close()
+    gravar_dados(f'{nome_album}%{ano_album}%{nome_banda}%Nao\n')
 
   telaCadastro.destroy()
 
-def solicitar():
+### Regra de Negócio
+def busca_por_artista():
     fatorDeBusca = busca.get().lower()
-    arquivo = open('dados.txt', 'r')
-    dados = arquivo.readlines()
     coletados = ''
-    for album in dados:
-        album = album.split('%')
-        nomeArtista = album[2].lower()
-        if fatorDeBusca in nomeArtista:
-            coletados += f'{album[0]} / {album[1]} / {album[2]}\n'
-    arquivo.close()
+    for album in buscar_dados():
+      nomeArtista = album[2].lower()
+      if fatorDeBusca in nomeArtista:
+          coletados += f'{album[0]} / {album[1]} / {album[2]}\n'
 
     resultadoDaBuscaArtista.configure(text=coletados)
 
-def buscar():
+### Regra de Negócio
+def busca_por_ano():
     select = opcao.get()
     data = int(tb_Data.get())
-    albuns = []
     albuns_filtrados = ''
 
-    arquivo = open('dados.txt', 'r')
-    dados = arquivo.readlines()
-    for album in dados:
-        album = album.split('%')
-        albuns.append(album)
-    arquivo.close()
-
     if select == 1:
-        for album in albuns:
+        for album in buscar_dados():
             if int(album[1]) < data:
                 albuns_filtrados += f'{album[0]} / {album[1]} / {album[2]}\n'
     elif select == 2:
-        for album in albuns:
+        for album in buscar_dados():
             if int(album[1]) > data:
                 albuns_filtrados += f'{album[0]} / {album[1]} / {album[2]}\n'
     else:
-        for album in albuns:
+        for album in buscar_dados():
             if int(album[1]) == data:
                 albuns_filtrados += f'{album[0]} / {album[1]} / {album[2]}\n'
 
     
     resultadoDaBuscaAno.configure(text=albuns_filtrados)
 
+### GUI
 def tela_dados_salvos():
   global lb_lancamento, lb_nomedabanda, lb_anolancamento, cadastrados, tb
 
@@ -79,24 +97,19 @@ def tela_dados_salvos():
   bg = PhotoImage(file='bg.png')
   Label(telaDadosSalvos, image=bg).pack()
 
-  ### Lendo e agrupando dados 
-  arquivo = open('dados.txt', 'r')
-  dados = arquivo.readlines()
-  nomes = []
-  cadastrados = {}
-  for album in dados:
-      album = album.split('%')
-      nomes.append(album[0])
-      cadastrados.setdefault(album[0], album[1:])
-  arquivo.close()
+  ### Agrupando Dados para Interface
+  data = coletar_albuns_cadastrados()
+  nomes = data[0]
+  cadastrados = data[1]
+  
 
   ### Lista de Dados na Interface
   Label(telaDadosSalvos, text='Álbuns Cadastrados', font='arial 18', fg='#32cd32', bg='#121212').place(x=140, y=50)
   tb = Combobox(telaDadosSalvos, value=nomes, font='arial 14')
   tb.place(x=80, y=125)
 
-  ### butao
-  Button(telaDadosSalvos, text='Verificar', font='arial 12', command=verificar, bg='#32cd32', fg='white').place(x=330, y=125)
+  ### Botão
+  Button(telaDadosSalvos, text='Verificar', font='arial 12', command=verificar_album, bg='#32cd32', fg='white').place(x=330, y=125)
 
   ### Ano de lancamento
   lb_anolancamento = Label(telaDadosSalvos, text='Ano de Lançamento: ', font='arial 14', fg='white', bg='#121212')
@@ -112,6 +125,7 @@ def tela_dados_salvos():
 
   telaDadosSalvos.mainloop()
 
+### GUI
 def tela_cadastro():
   global txtalb, txtano, txtband, opcaoLancamento, telaCadastro
   telaCadastro = Toplevel()
@@ -157,6 +171,7 @@ def tela_cadastro():
 
   telaCadastro.mainloop()
 
+### GUI
 def tela_busca_por_artista():
     global busca, resultadoDaBuscaArtista
 
@@ -171,15 +186,15 @@ def tela_busca_por_artista():
     busca = Entry(telaBuscaArtista, font='arial 14', bg='#272727', fg='whitesmoke', bd=0,)
     busca.place(x=90, y=95, height=40)
 
-    Button(telaBuscaArtista, text='Buscar', command=solicitar, font='arial 16', bg='#32cd32', fg='white').place(x=320, y=95)
+    Button(telaBuscaArtista, text='Buscar', command=busca_por_artista, font='arial 16', bg='#32cd32', fg='white').place(x=320, y=95)
     Label(telaBuscaArtista, text='ÁLBUM /// ANO-DE-LANÇAMENTO /// ARTISTA', font='arial 13', fg='#32cd32', bg='#121212').place(x=60,y=160)
 
     resultadoDaBuscaArtista = Label(telaBuscaArtista, font='arial 12', text='', justify='left', fg='whitesmoke', bg='#121212')
     resultadoDaBuscaArtista.place(x=60, y=210)
 
-
     telaBuscaArtista.mainloop()
 
+### GUI
 def tela_busca_por_ano():
     global opcao, tb_Data, resultadoDaBuscaAno
 
@@ -218,7 +233,7 @@ def tela_busca_por_ano():
     tb_Data.place(x=60, y=140)
 
 
-    Button(telaBuscaAno, command=buscar, text='BUSCAR', font='arial 13', fg='white', bg='#32cd32').place(x=320, y=135)
+    Button(telaBuscaAno, command=busca_por_ano, text='BUSCAR', font='arial 13', fg='white', bg='#32cd32').place(x=320, y=135)
 
     Label(telaBuscaAno, text='ÁLBUM /// ANO-DE-LANÇAMENTO /// ARTISTA', font='arial 13', fg='#32cd32', bg='#121212').place(x=60,y=185)
 
@@ -227,6 +242,7 @@ def tela_busca_por_ano():
 
     telaBuscaAno.mainloop()
 
+### GUI
 def criarTelaInicial():
   telaInicial = Tk()
   telaInicial.geometry('500x500')
